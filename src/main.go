dbafecb59be128ef8af385cf45bebf7dba5dc967
Yunus Emre Manav <yunusemremanav@gmail.com>
2026-06-18 21:46:14 +0300

    Command line arguments testing

--- src/main.go
+++ src/main.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 )
+
+type LogLevel string
+
+const (
+	LevelInfo    LogLevel = "INFO"
+	LevelWarning LogLevel = "WARNING"
+	LevelError   LogLevel = "ERROR"
+)
+
+type LogEntry struct {
+	Timestamp time.Time
+	Level     LogLevel
+	Message   string
+}
+
+type LogFile struct {
+	Entries []LogEntry
+	Path    string
+}
 
 func main() {
 
 	args := os.Args
+	argc := len(args)
+	if argc != 2 {
+		log.Fatalf("Invalid number of arguments")
+	}
+
+	fmt.Println("Command Line Arguments")
 	for _, arg := range args {
 		fmt.Println(arg)
-		// fmt.Println(i)
 	}
 
 }
