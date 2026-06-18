5a7ae3f60ae5716ed37c3b46fc1cec74a0bd1b05
Yunus Emre Manav <yunusemremanav@gmail.com>
2026-06-18 23:48:04 +0300

    Each line is parsed into a LogEntry, TEST_CASES.md is moved into test/

--- src/main.go
+++ src/main.go
@@ -5,12 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 )
 
 type LogLevel string
-
-const StringSliceCapacity = 100
 
 const (
 	LevelInfo    LogLevel = "INFO"
@@ -29,7 +28,7 @@ type LogFile struct {
 	Path    string
 }
 
-func ReadFile(FileName string) {
+func ReadFile(FileName string) []string {
 
 	file, err := os.Open(FileName)
 	if err != nil {
@@ -38,17 +37,69 @@ func ReadFile(FileName string) {
 
 	fileScanner := bufio.NewScanner(file)
 	fileScanner.Split(bufio.ScanLines)
-	var fileLines []string = make([]string, StringSliceCapacity)
+	var fileLines []string
 
 	for fileScanner.Scan() {
 		fileLines = append(fileLines, fileScanner.Text())
 	}
 
-	for _, line := range fileLines {
-		fmt.Println(line)
-	}
+	// for _, line := range fileLines {
+	// 	fmt.Println(line)
+	// }
 	// fmt.Println(string(data))
+	return fileLines
+}
 
+func CheckLine(Line string) LogEntry {
+
+	var entry LogEntry
+	// Line[0] should be '['
+	// Line [20] should be ']'
+	// Check Timestamp
+	timeStampStr := Line[0:21]
+	fmt.Println(timeStampStr)
+	if timeStampStr[0] != '[' && timeStampStr[20] != ']' {
+		panic("timestamp is not in correct format ")
+	}
+	timeStampStr = strings.Trim(timeStampStr, "[]")
+	t, err := time.Parse("2006-01-02 15:04:05", timeStampStr)
+	if err != nil {
+		panic(err)
+	}
+	entry.Timestamp = t
+
+	// After Stimestamp
+	remainingLine := Line[22:]
+	fmt.Println(remainingLine)
+
+	strings := strings.SplitN(remainingLine, " ", 2)
+	// fmt.Printf("Level: \"%s\"| Message: \"%s\"", strings[0], strings[1])
+	entry.Level = LogLevel(strings[0])
+	entry.Message = strings[1]
+	// for i := 0; i < len(Line); i++ {
+	// 	// fmt.Printf("%d, %c\n", i, Line[i])
+	// }
+
+	return entry
+}
+
+func ParseLines(FileLines []string) []LogEntry {
+
+	lineCount := len(FileLines)
+	entries := make([]LogEntry, lineCount)
+	entry := CheckLine(FileLines[0])
+	fmt.Println("timestamp:", entry.Timestamp)
+	fmt.Println("level:", entry.Level)
+	fmt.Println("message:", entry.Message)
+	// Parsing logic
+	// for _, line := range FileLines {
+	//
+	// 	// fmt.Printf("line: %d, log: %s\n", i, line)
+	// 	// var entry LogEntry
+	// 	CheckLine(line)
+	// }
+
+	return entries
 }
 
 func main() {
@@ -59,13 +110,15 @@ func main() {
 		log.Fatalf("Invalid number of arguments")
 	}
 
-	fmt.Println("Command Line Arguments")
+	// fmt.Println("Command Line Arguments")
 	// for _, arg := range args {
 	// 	fmt.Println(arg)
 	// }
 
-	fileName := args[1]
-	fmt.Println("Filename: " + fileName)
+	var logfile LogFile
+	logfile.Path = args[1]
+	// fmt.Println("Filename: " + fileName)
 
-	ReadFile(fileName)
+	logfile.Entries = ParseLines(ReadFile(logfile.Path))
+
 }
